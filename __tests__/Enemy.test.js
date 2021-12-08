1770e1c131d623c09b54e914551badf1fa682676
trip6love <lanebyers13@gmail.com>
2021-12-08 15:46:24 -0500

    Game function

--- __tests__/Enemy.test.js
+++ __tests__/Enemy.test.js
@@ -50,3 +50,10 @@ test("subtracts from enemy's health", () => {
   
     expect(enemy.health).toBe(0);
 });
+
+test('gets a description of the enemy', () => {
+    const enemy = new Enemy('goblin', 'sword');
+  
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('goblin'));
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'));
+});
